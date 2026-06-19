7ec02a58a8365bbf23299b8fb5a6c0f9c186a62a
joostvankempen11 <joostvankempen11@gmail.com>
2026-06-19 11:00:09 +0200

    Trigger republish in lovable dev environment

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@
 // You can pass additional config via defineConfig({ vite: { ... }, etc... }) if needed.
 import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
-
+// Trigger republish
 export default defineConfig({
   tanstackStart: {
     ssr: false,
